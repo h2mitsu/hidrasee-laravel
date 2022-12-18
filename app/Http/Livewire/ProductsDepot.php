04e7843b0d4cf4e1262609767cedc13f35d7463b
h1mitsu <115644212+Zahraarif@users.noreply.github.com>
2022-12-18 16:28:05 +0700

    Fixed Depot list

--- app/Http/Livewire/ProductsDepot.php
+++ app/Http/Livewire/ProductsDepot.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Depot;
+use App\Models\Kota;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductsDepot extends Component
 {
+    use WithPagination;
+
+    public $search, $depot;
+
+    protected $updatesQueryString = ['search'];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function mount($depotid) {
+        $depotDetail = Depot::find($depotid);
+
+        if($depotDetail) {
+            $this->depot= $depotDetail;
+        }
+    }
+
     public function render()
     {
-        return view('livewire.products-depot');
+        if ($this->search) {
+            $products = Product::where('depot_id', $this->depot->id)->where('nama_produk', 'like', '%' . $this->search . '%')->paginate(12);
+        } else {
+            $products = Product::where('depot_id', $this->depot->id)->paginate(12); //12 is the number of products per page
+        }
+
+        return view('livewire.product-index', [
+            'products' => $products
+        ]);
     }
 }
